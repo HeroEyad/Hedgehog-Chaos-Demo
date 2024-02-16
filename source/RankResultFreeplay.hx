b7b40f52c8ac35cebee771f0ed5e0e4b045e8b0e
Hero <68159082+HeroEyad@users.noreply.github.com>
2024-02-17 00:17:55 +0200

    zzz

---
+++ source/RankResultFreeplay.hx
@@ -0,0 +1,13 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+
+using StringTools;
+
+class RankResultFreeplay extends MusicBeatState {
+// later..
+}
